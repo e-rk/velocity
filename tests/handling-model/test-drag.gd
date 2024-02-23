1eea8df9e7d5d05d1b55131f7ad37fc8c2dc42f1
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2024-02-23 21:40:24 +0100

    tests: add drag tests
    Added data set and CSV test for drag implementation.

---
+++ tests/handling-model/test-drag.gd
@@ -0,0 +1,32 @@
+extends CsvTest
+
+var model: HandlingModelRE
+
+@onready var car: Car = preload("res://import/cars/B911/B911.glb").instantiate()
+@onready var performance: CarPerformance = car.performance
+
+const EPSILON = 0.0001
+
+
+func before_all():
+	self.model = HandlingModelRE.new()
+
+
+func get_csv() -> FileAccess:
+	return FileAccess.open("res://tests/handling-model/data/drag.csv", FileAccess.READ)
+
+
+func make_params(data: Dictionary) -> Dictionary:
+	var result = Dictionary()
+	result["performance"] = self.performance
+	result["basis_to_road"] = Basis()
+	result["linear_velocity"] = self.local_linear_velocity(data)
+	return result
+
+
+func body(data: Dictionary):
+	var params = self.make_params(data)
+	var expected = float(data["result"])
+	var result = self.model.drag(params)
+	var msg = "v=" + str(params["linear_velocity"])
+	assert_almost_eq(result, expected, EPSILON, msg)
