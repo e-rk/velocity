2445c190798b9edfe6ddcaffb3a49bcaac05f421
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2024-02-23 23:58:03 +0100

    tests: add CSV tests for downforce factor

---
+++ tests/handling-model/test-wheel-downforce-factor.gd
@@ -0,0 +1,41 @@
+extends CsvTest
+
+var model: HandlingModelRE
+
+@onready var car: Car = preload("res://import/cars/B911/B911.glb").instantiate()
+@onready var performance: CarPerformance = car.performance
+
+const EPSILON = 0.0001
+
+
+func before_all():
+	self.model = HandlingModelRE.new()
+
+
+func get_csv() -> FileAccess:
+	return FileAccess.open(
+		"res://tests/handling-model/data/wheel_downforce_factor.csv", FileAccess.READ
+	)
+
+
+func make_params(data: Dictionary) -> Dictionary:
+	var result = Dictionary()
+	result["performance"] = self.performance
+	result["basis_to_road"] = Basis()
+	result["linear_velocity"] = self.local_linear_velocity(data)
+	return result
+
+
+func body(data: Dictionary):
+	var params = self.make_params(data)
+	var wheels = [
+		{"type": CarTypes.Wheel.FRONT_LEFT},
+		{"type": CarTypes.Wheel.FRONT_RIGHT},
+		{"type": CarTypes.Wheel.REAR_LEFT},
+		{"type": CarTypes.Wheel.REAR_RIGHT},
+	]
+	for i in wheels.size():
+		var expected = self.wheel_downforce(data, i)
+		var result = self.model.wheel_downforce_factor(params, wheels[i])
+		var msg = "v.z=" + str(params["linear_velocity"].z) + " type=" + str(wheels[i]["type"])
+		assert_almost_eq(result, expected, EPSILON, msg)
