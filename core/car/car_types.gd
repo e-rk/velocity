75d34b284c1fc03086aba561c2187e5c0a14583a
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2024-02-23 20:31:30 +0100

    car: add common car types
    Added Gear and Wheel enum types.

---
+++ core/car/car_types.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name CarTypes
+
+enum Gear {
+	REVERSE,
+	NEUTRAL,
+	GEAR_1,
+	GEAR_2,
+	GEAR_3,
+	GEAR_4,
+	GEAR_5,
+	GEAR_6,
+}
+
+enum Wheel {
+	FRONT_RIGHT,
+	FRONT_LEFT,
+	REAR_RIGHT,
+	REAR_LEFT
+}
