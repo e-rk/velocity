e89a4dc4baa0e9778089f090b157f94ff65684dc
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2024-02-24 18:00:15 +0100

    db: add track and car database scenes
    The database scenes can be used to look-up and query information about
    cars and tracks.

---
+++ core/db/track_db.gd
@@ -0,0 +1,29 @@
+extends Node
+class_name TrackDatabase
+
+signal database_updated;
+
+@onready var watcher = %DirectoryWatcher
+
+var tracks: Dictionary = {}
+
+class TrackData:
+	var name: String
+	var path: String
+	var uuid: String
+
+func get_track_by_uuid(uuid: String) -> TrackData:
+	return tracks[uuid]
+
+func _on_directory_watcher_content_changed():
+	var tracks = {}
+	for file in watcher.files:
+		var path = "%s/%s/%s.tscn" % [watcher.directory, file, file]
+		var uuid = path
+		var car = TrackData.new()
+		car.name = file
+		car.path = path
+		car.uuid = uuid
+		tracks[uuid] = car
+	self.tracks = tracks
+	self.database_updated.emit()
