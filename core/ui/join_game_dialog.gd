900d087754279f04bfd7a31f6475193feb60d625
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2024-02-24 22:29:42 +0100

    ui: add join dialog scene
    Added scene allowing the user to pick a server and join the game.

---
+++ core/ui/join_game_dialog.gd
@@ -0,0 +1,22 @@
+extends PopupPanel
+
+signal server_selected
+
+@onready var server_address = %ServerAddress
+@onready var server_list = %ServerList
+
+func _ready():
+	server_list.set_item_metadata(0, "localhost")
+
+func _on_cancel_button_pressed():
+	self.hide()
+
+
+func _on_join_button_pressed():
+	self.server_selected.emit(server_address.text)
+	self.hide()
+
+
+func _on_server_list_item_selected(index):
+	var metadata = server_list.get_item_metadata(index)
+	server_address.text = metadata
