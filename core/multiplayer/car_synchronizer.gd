588be13559d9e9fb7c82257745a1ff6f8fb51af4
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2024-02-25 21:12:41 +0100

    multiplayer: add car synchronizer class
    Class synchronizer class receives car positions from the authority and
    adjusts the physics state on the local peer.

---
+++ core/multiplayer/car_synchronizer.gd
@@ -0,0 +1,29 @@
+class_name CarSynchronizer
+extends MultiplayerSynchronizer
+
+@onready var target: Car = get_node(root_path)
+@onready var body_state: PhysicsDirectBodyState3D = PhysicsServer3D.body_get_direct_state(target.get_rid())
+
+@export var sync_state: Dictionary = {
+	"transform": Transform3D.IDENTITY,
+	"linear_velocity": Vector3.ZERO,
+	"angular_velocity": Vector3.ZERO,
+}
+
+func _ready():
+	self.synchronized.connect(self._on_synchronized)
+	self.target.set_multiplayer_authority(str(target.name).to_int())
+	self.set_multiplayer_authority(1)
+
+func _physics_process(delta: float):
+	if multiplayer.get_unique_id() == 1:
+		sync_state = {
+			"transform": body_state.transform,
+			"linear_velocity": body_state.linear_velocity,
+			"angular_velocity": body_state.angular_velocity,
+		}
+
+func _on_synchronized():
+	body_state.transform = sync_state["transform"]
+	body_state.linear_velocity = sync_state["linear_velocity"]
+	body_state.angular_velocity = sync_state["angular_velocity"]
