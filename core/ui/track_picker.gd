e83f18a829c4d5d5cfeb5fc4094775b55ddf0230
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2024-02-25 01:16:55 +0100

    ui: external modifications of track picker
    The selected track must be synchronized across the peers. Therefore the
    option button selection must able to be modified externally.

--- core/ui/track_picker.gd
+++ core/ui/track_picker.gd
@@ -2,9 +2,21 @@ extends HBoxContainer
 class_name TrackPicker
 
 signal track_selected
-@export var selected_track: String = ""
+@export var selected_track: String = "":
+	set(value):
+		selected_track = value
+		self._on_selection_externally_changed()
+	get:
+		return selected_track
 
-@onready var track_selection = $TrackSelection
+@export var disabled: bool = false:
+	set(value):
+		disabled = value
+		self._on_disable_state_changed()
+	get:
+		return disabled
+
+@onready var track_selection: OptionButton = $TrackSelection
 
 func _ready():
 	TrackDB.database_updated.connect(self._on_track_database_updated)
@@ -22,3 +34,16 @@ func _on_track_database_updated():
 func _on_track_selection_item_selected(index):
 	selected_track = track_selection.get_item_metadata(index)
 	self.track_selected.emit()
+
+
+func _on_disable_state_changed():
+	for i in track_selection.item_count:
+		track_selection.set_item_disabled(i, self.disabled)
+
+
+func _on_selection_externally_changed():
+	for i in track_selection.item_count:
+		var track = track_selection.get_item_metadata(i)
+		if track == self.selected_track:
+			track_selection.selected = i
+			break
