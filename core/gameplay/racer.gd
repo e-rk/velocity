3783464e084b5f2b66a6ef432765e796fdc624b0
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2024-02-27 23:47:13 +0100

    gameplay: add racer class
    Racer class tracks progress along the track, completed laps and lap
    time.

---
+++ core/gameplay/racer.gd
@@ -0,0 +1,33 @@
+class_name Racer
+extends Node
+
+var laps: int = 0:
+	set(value):
+		if value > max_laps:
+			_on_lap_finished(value)
+		laps = value
+		max_laps = max(max_laps, laps)
+	get:
+		return laps
+
+var max_laps := 0
+var car: Car
+var racer_name: String = ""
+var current_lap_time := 0.0
+var last_lap_time := 0.0
+var track_progress := 0.80
+
+
+func _init(car: Car):
+	self.car = car
+	self.add_child(car)
+
+
+func _process(delta: float):
+	current_lap_time += delta
+
+
+func _on_lap_finished(laps: int):
+	if laps > 1:
+		last_lap_time = current_lap_time
+		current_lap_time = 0.0
