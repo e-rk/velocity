0cec69242a9ce5900c4d99770417e28088187acd
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2024-03-03 16:25:20 +0100

    ui: modify track picker to use signal on external change
    The track picker will now emit a signal when a selected track was
    changed externally without user input.

--- core/ui/track_picker.gd
+++ core/ui/track_picker.gd
@@ -2,10 +2,13 @@ extends HBoxContainer
 class_name TrackPicker
 
 signal track_selected
+signal track_externally_changed
+signal track_not_found
+
 @export var selected_track: String = "":
 	set(value):
 		selected_track = value
-		self._on_selection_externally_changed()
+		self.track_externally_changed.emit()
 	get:
 		return selected_track
 
@@ -42,7 +45,7 @@ func _on_disable_state_changed():
 		track_selection.set_item_disabled(i, self.disabled)
 
 
-func _on_selection_externally_changed():
+func _on_track_externally_changed():
 	for i in track_selection.item_count:
 		var track = track_selection.get_item_metadata(i)
 		if track == self.selected_track:
