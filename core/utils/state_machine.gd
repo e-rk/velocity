f87dd3513548c540c7f60075bdee5e34e930ba5e
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2024-03-15 23:37:32 +0100

    utils: state machine calls enter on _ready

--- core/utils/state_machine.gd
+++ core/utils/state_machine.gd
@@ -8,9 +8,17 @@ extends Node
 signal state_changed
 
 
+func _ready():
+	current_state.enter()
+
+
 func set_state(next_state: State):
 	assert(next_state != null)
 	self.current_state.leave()
 	self.current_state = next_state
 	self.current_state.enter()
 	self.state_changed.emit(self.current_state.name)
+
+
+func is_state_active(expected_state) -> bool:
+	return expected_state == self.current_state
