62329f684609feb41d8a3ae25f2e19c81cddd932
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2026-03-14 18:22:20 +0100

    spectator: typed assignment

--- core/gameplay/single_race_player_spectator.gd
+++ core/gameplay/single_race_player_spectator.gd
@@ -6,8 +6,7 @@ extends "res://core/gameplay/player_spectator.gd"
 
 
 func _update_ui(player: Player):
-	var racer = player.get_node("Racer") as Racer
+	var racer := player.get_node_or_null("Racer") as Racer
 	ui.set_laps(min(racer.laps, self.race_laps), self.race_laps)
 	lap_timer.set_current_time(racer.current_lap_time())
 	lap_timer.set_last_time(racer.get_best_lap_time())
-	
